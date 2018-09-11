41bda22c9a48e6e048541fb25643904fd6ce65c4
Fleford Redoloza <fleford@gmail.com>
2018-09-11 00:46:23 -0600

    added float conversion

--- EO_Aberdeen/GWM_Manipulator.py
+++ EO_Aberdeen/GWM_Manipulator.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 start_string1 = "OPTIMAL RATES FOR EACH FLOW VARIABLE"
 start_string2 = "Q1"
@@ -7,11 +8,14 @@ line_cnt = 0
 start_string1_flag = False
 print_flag = False
 
+well_array_contribution = np.array([[]])
+
 with open("supply2.gwmout.parallel", 'r') as f:
 
     for line in f:
         line_cnt = line_cnt + 1
 
+        # Look for when the optimal solution table starts and stops
         if start_string1 in line:
             start_string1_flag = True
         if start_string1_flag and start_string2 in line:
@@ -20,5 +24,11 @@ with open("supply2.gwmout.parallel", 'r') as f:
             print_flag = False
             start_string1_flag = False
 
+        # Extract Values from "Contribution to objective"
         if print_flag:
-            print(line)
+            # print(line, end="")
+            line_array = line.split()
+            well_array = np.append(well_array, float(line_array[2]))
+
+well_array = well_array.reshape(-1, 1)
+print(well_array)
