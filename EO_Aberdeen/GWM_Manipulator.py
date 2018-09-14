4f0347321290bccaedeabd21ebad67655c3a2786
fleford <fleford@gmail.com>
2018-09-14 13:30:51 -0600

    wrote function to extract results from gwmout
    Signed-off-by: fleford <fleford@gmail.com>

--- EO_Aberdeen/GWM_Manipulator.py
+++ EO_Aberdeen/GWM_Manipulator.py
@@ -1,34 +1,52 @@
 import numpy as np
 
-start_string1 = "OPTIMAL RATES FOR EACH FLOW VARIABLE"
-start_string2 = "Q1"
-end_string = "------------        ------------        ------------"
-# starter_string = "Q1    "
-line_cnt = 0
-start_string1_flag = False
-print_flag = False
 
-well_array_contribution = np.array([[]])
+def extract_contributions():
+    # Looks at the GWM out file and extracts well names and contributions
 
-with open("supply2.gwmout.parallel", 'r') as f:
+    start_string1 = "OPTIMAL RATES FOR EACH FLOW VARIABLE"
+    start_string2 = "Q1"
+    end_string = "------------        ------------        ------------"
+    # starter_string = "Q1    "
+    line_cnt = 0
+    start_string1_flag = False
+    print_flag = False
 
-    for line in f:
-        line_cnt = line_cnt + 1
+    well_array_contributions = np.array([])
+    well_array_names = []
 
-        # Look for when the optimal solution table starts and stops
-        if start_string1 in line:
-            start_string1_flag = True
-        if start_string1_flag and start_string2 in line:
-            print_flag = True
-        elif end_string in line:
-            print_flag = False
-            start_string1_flag = False
+    with open("supply2.gwmout.parallel", 'r') as f:
 
-        # Extract Values from "Contribution to objective"
-        if print_flag:
-            # print(line, end="")
+        for line in f:
+            line_cnt = line_cnt + 1
+
+            # Look for when the optimal solution table starts and stops
+            if start_string1 in line:
+                start_string1_flag = True
+            if start_string1_flag and start_string2 in line:
+                print_flag = True
+            elif end_string in line:
+                print_flag = False
+                start_string1_flag = False
+
+            # Split line into an array
             line_array = line.split()
-            well_array = np.append(well_array, float(line_array[2]))
 
-well_array = well_array.reshape(-1, 1)
-print(well_array)
+            # Extract Contributions from "Contribution to objective"
+            if print_flag:
+                well_array_contributions = np.append(well_array_contributions, np.array([float(line_array[2])]), axis=0)
+
+            # Extract names of well parameters
+            if print_flag:
+                well_array_names.append(line_array[0])
+
+    return well_array_names, well_array_contributions
+
+
+well_names, well_contributions = extract_contributions()
+print(well_names)
+print()
+print(well_contributions)
+
+
+# Combine well names with contribution values ?
