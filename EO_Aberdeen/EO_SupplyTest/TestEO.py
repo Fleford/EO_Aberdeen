8a33db002766936df3cd3a3fcfd916779caad0ce
fleford <fleford@gmail.com>
2018-09-25 12:40:08 -0600

    renamed folders
    Signed-off-by: fleford <fleford@gmail.com>

---
+++ EO_Aberdeen/EO_SupplyTest/TestEO.py
@@ -0,0 +1,96 @@
+from EO_Aberdeen.EO_SupplyTest import EO
+import numpy as np
+
+avoided_points = np.array([[20, 20],
+                           [80, 80],
+                           [20, 80]])
+
+
+def calculate_fitness(self):
+    # Parse out parameter matrix
+    parameters = self.parameters()
+
+    # Generate new fitness vector
+    center_point = np.array([50, 50])
+    fitness = np.linalg.norm(parameters - center_point, axis=1) * self.maximize
+
+    return fitness
+
+
+def iterate(self):
+    self.remove_weakest()
+    self.append_row(self.generate_row())
+    self.update_fitness(calculate_fitness(self))
+    self.update_best()
+
+
+# Testing the EO_Solution class
+sol1 = EO.EO(10, False, avoid_list=avoided_points)
+print(sol1.solution)
+print(sol1.fitness_ready)
+print()
+
+print("Update fitness")
+sol1.update_fitness(calculate_fitness(sol1))
+print(sol1.solution)
+print(sol1.total_fitness())
+print("Fitness ready?", sol1.fitness_ready)
+print(sol1.eval_count)
+print()
+
+print("Best Sol")
+sol1.update_best()
+print(sol1.best_solution.solution)
+print(sol1.best_solution.total_fitness())
+print(sol1.best_solution.fitness_ready)
+print()
+
+print("remove_weakest")
+sol1.remove_weakest()
+print(sol1.solution)
+print("Replaced row: ", sol1.replaced_row)
+print()
+
+print("generate_row")
+row = sol1.generate_row()
+print(row)
+print()
+
+print("append_row")
+sol1.append_row(row)
+print(sol1.solution)
+print(sol1.fitness_ready)
+print(sol1.total_fitness())
+print()
+
+print("Update fitness")
+sol1.update_fitness(calculate_fitness(sol1))
+print(sol1.solution)
+print(sol1.total_fitness())
+print(sol1.fitness_ready)
+print(sol1.eval_count)
+print()
+
+print("Best Sol")
+sol1.update_best()
+print(sol1.best_solution.solution)
+print(sol1.best_solution.total_fitness())
+print(sol1.best_solution.fitness_ready)
+print()
+
+iterate(sol1)
+print(sol1.solution)
+print(sol1.total_fitness())
+print()
+
+print("Testing generate_parameter")
+print(sol1.generate_parameter())
+print()
+
+print("Testing generate_possible_point")
+print(EO.generate_possible_point(10, 100, 20, 100))
+print()
+
+print("Testing generate_initial_array")
+print(EO.generate_initial_array(0, 100, 0, 100, 5, 1, avoid_list=avoided_points))
+print()
---
+++ EO_Aberdeen/EO_SupplyTest/TestEO_Plot.py
@@ -0,0 +1,52 @@
+from EO_Aberdeen.EO_SupplyTest import EO
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import numpy as np
+
+
+# Prepare avoided points
+avoided_points = np.array([[50, 60],
+                           [50, 55],
+                           [50, 50]])
+
+# Prepare EO instance
+sol1 = EO.EO(10, False, min_dist=3, avoid_list=avoided_points)
+
+# Prepare plot instance
+fig, ax = plt.subplots()
+
+
+def calculate_fitness(self):
+    # Parse out parameter matrix
+    parameters = self.parameters()
+
+    # Generate new fitness vector
+    center_point = np.array([50, 50])
+    fitness = np.linalg.norm(parameters - center_point, axis=1) * self.maximize
+
+    return fitness
+
+
+def iterate(self):
+    self.remove_weakest()
+    self.append_row(self.generate_row())
+    self.update_fitness(calculate_fitness(self))
+    self.update_best()
+
+
+# Iterates and plots EO
+def update(i):
+    # Iterate through EO
+    iterate(sol1)
+
+    # Plot result
+    ax.clear()
+    ax.plot(sol1.solution[:, 1], sol1.solution[:, 2], 'bo')
+    ax.plot(sol1.avoid_list[:, 0], sol1.avoid_list[:, 1], 'ro')
+    ax.set_title("Gen = {}, BestSum = {}".format(i, sol1.total_fitness()))
+    plt.axis([0, 100, 0, 100])
+
+
+# Animation
+ani = animation.FuncAnimation(fig, update, interval=500)
+plt.show()
