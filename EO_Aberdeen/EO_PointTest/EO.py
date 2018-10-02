289f5686a1a1ae1f10d042b4ddb27b43c01e45db
fleford <fleford@gmail.com>
2018-10-02 17:54:44 -0600

    ran E_Supply2 with the 2nd worst point as the pivot
    Signed-off-by: fleford <fleford@gmail.com>

--- EO_Aberdeen/EO_PointTest/EO.py
+++ EO_Aberdeen/EO_PointTest/EO.py
@@ -1,5 +1,6 @@
-import numpy as np
 import copy
+
+import numpy as np
 
 
 class EO(object):
@@ -156,8 +157,8 @@ class EO(object):
         # Scale random unit vector by the rand_dist
         u = rand_dist * u
 
-        # Return a new point using lowest fit row
-        new_point = parameters[-1] + np.random.rand() * u * 1
+        # Return a new point using lowest fit row, [0]
+        new_point = parameters[0] + np.random.rand() * u * 1
         # new_point = parameters[np.random.randint(parameters_rows)] + np.random.rand() * u
 
         # Clip new point to boundary and round components
