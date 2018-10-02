913ca106460552dd5d6d16ef027ab8a79d00703e
fleford <fleford@gmail.com>
2018-10-02 17:56:23 -0600

    ran E_Supply2 with the best point as the pivot
    Signed-off-by: fleford <fleford@gmail.com>

--- EO_Aberdeen/EO_PointTest/EO.py
+++ EO_Aberdeen/EO_PointTest/EO.py
@@ -158,7 +158,7 @@ class EO(object):
         u = rand_dist * u
 
         # Return a new point using lowest fit row, [0]
-        new_point = parameters[0] + np.random.rand() * u * 1
+        new_point = parameters[-1] + np.random.rand() * u * 1
         # new_point = parameters[np.random.randint(parameters_rows)] + np.random.rand() * u
 
         # Clip new point to boundary and round components
