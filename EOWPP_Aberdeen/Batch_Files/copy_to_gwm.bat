89984b23b9a255be00709c880eb14d25e6289311
Fleford Redoloza <fleford@gmail.com>
2018-11-29 00:10:05 -0700

    modified copy_to_gwm to copy over a clean well file

--- EOWPP_Aberdeen/Batch_Files/copy_to_gwm.bat
+++ EOWPP_Aberdeen/Batch_Files/copy_to_gwm.bat
@@ -1,8 +1,8 @@
 @echo off
 REM This batch file copies files from the python script to the GWM folder
 REM Note that this program assumes the root is where this batch file was called from
-COPY .\abr.well .\AberdeenModel\model\tr
-COPY .\abr.well .\AberdeenModel\Runner1\tr
-COPY .\abr.well .\AberdeenModel\Runner2\tr
+COPY .\abr.wel .\AberdeenModel\model\tr
+COPY .\abr.wel .\AberdeenModel\Runner1\tr
+COPY .\abr.wel .\AberdeenModel\Runner2\tr
 COPY .\abr.decvar .\AberdeenModel\model\tr
 COPY .\abr.hedcon .\AberdeenModel\model\tr
