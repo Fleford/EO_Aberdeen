2035087b0206366b8fa48a1a48da9b6a3b9be1e8
Fleford Redoloza <fleford@gmail.com>
2019-02-01 00:36:46 -0700

    made image for synth hk

--- EOWPP_Aberdeen/txt_to_array.py
+++ EOWPP_Aberdeen/txt_to_array.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+import matplotlib
 
 def txt_to_array(filepath):
     try:
@@ -20,20 +20,38 @@ def txt_to_array(filepath):
         return np.loadtxt("col_fixed_" + filepath)
 
 
-hkx = txt_to_array("abr2_kx.txt")
-mask = txt_to_array("ib2_ref.dat")
-print(mask.shape, hkx.shape)
-array = np.ma.masked_where(mask == 0, hkx)
-print(array)
+SynthK = txt_to_array("SynthHeteroK.txt")
+print(SynthK, SynthK.shape)
+# hkx = txt_to_array("abr2_kx.txt")
+# mask = txt_to_array("ib2_ref.dat")
+# print(mask.shape, hkx.shape)
+# array = np.ma.masked_where(mask == 0, hkx)
+# print(array)
 
-# plt.imshow(array, cmap="Purples", extent=[300, 100, 300, 100])
-plt.imshow(array, cmap="jet", alpha=0.8)
-plt.colorbar()
-plt.plot(200, 116, "gx")
-plt.annotate(1.0, (200, 116))
-plt.plot(298, 100, "g.")
-plt.plot(178, 117, "r.")
+my_cmap = matplotlib.colors.ListedColormap(['r', 'b'])
+# # plt.imshow(array, cmap="Purples", extent=[300, 100, 300, 100])
+plt.imshow(SynthK, cmap=my_cmap)
+cbar = plt.colorbar()
+
+cbar.ax.get_yaxis().set_ticks([])
+for j, lab in enumerate(['$50$', '$500$']):
+    cbar.ax.text(1.5, (j * 0.5) + 0.25, lab, ha='center', va='center', rotation=270)
+cbar.ax.get_yaxis().labelpad = 25
+cbar.ax.set_ylabel('Horizontal Hydraulic Conductivity', rotation=270)
+for row in range((SynthK.shape[0])):
+    plt.axhline(row + 0.5, lw=1, color='k', zorder=5)
+plt.yticks([])
+for x in range((SynthK.shape[1])):
+    plt.axvline(x + 0.5, lw=1, color='k', zorder=5)
+plt.xticks([])
+
+plt.xlabel("Model Columns (1 cell = 200 ft)")
+plt.ylabel("Model Rows (1 cell = 200 ft)")
+# plt.plot(200, 116, "gx")
+# plt.annotate(1.0, (200, 116))
+# plt.plot(298, 100, "g.")
+# plt.plot(178, 117, "r.")
 # plt.xlim(100, 300)
 # plt.ylim(300, 100)
-plt.savefig('Layer2_kx.pdf')
+# # plt.savefig('Layer2_kx.pdf')
 plt.show()
