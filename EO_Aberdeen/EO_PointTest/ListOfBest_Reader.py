8d0f464105831f9a11aef9c7f9adec0a5451b998
Fleford Redoloza <fleford@gmail.com>
2019-03-02 15:55:45 -0700

    wrote ListOfBest_Reader

--- EO_Aberdeen/EO_PointTest/ListOfBest_Reader.py
+++ EO_Aberdeen/EO_PointTest/ListOfBest_Reader.py
@@ -13,3 +13,11 @@ with open(filepath, "r") as f:
             min_len = line_len
 print(min_len)
 
+# Import values into a matrix
+# Values that exceed the minimum length are truncated
+with open(filepath, "r") as f:
+    for i, line in enumerate(f):
+        line_array = line.split()
+        new = list(map(float, line_array))
+        run = new[0:min_len+1]
+        print(run)
