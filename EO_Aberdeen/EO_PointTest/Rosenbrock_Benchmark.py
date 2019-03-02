55aa905928501bf5fa846bf2a94a9c86126fe08c
Fleford Redoloza <fleford@gmail.com>
2019-03-02 14:27:42 -0700

    wrote rosenbrock function

--- EO_Aberdeen/EO_PointTest/Rosenbrock_Benchmark.py
+++ EO_Aberdeen/EO_PointTest/Rosenbrock_Benchmark.py
@@ -2,9 +2,16 @@ import math
 import numpy as np
 
 
-def rosenbrock_2d(x, y, a, b):
-    return (a - x)**2 + b(y - x**2)**2
+def rosensbrock_multi_2d(x_array):
+    x_array = np.asarray(x_array)
+    x_array = x_array.reshape(-1, 2)
+
+    def rosenbrock_2d(x, y, a=1, b=100):
+        # Minimum at
+        return (a - x) ** 2 + b * (y - x ** 2) ** 2
+
+    return sum(rosenbrock_2d(x_array[:, 0], x_array[:, 1]))
 
 
-xl = [0, 0]
-print(rastrigin(xl, 10))
+xl = [1, 1, 1, 1]
+print(rosensbrock_multi_2d(xl))
