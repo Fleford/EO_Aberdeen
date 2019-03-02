22d5a60cf8cb89c752b1e42b04c211c9b3996491
Fleford Redoloza <fleford@gmail.com>
2019-03-02 15:35:16 -0700

    wrote ListOfBest_Reader

--- EO_Aberdeen/EO_PointTest/ListOfBest_Reader.py
+++ EO_Aberdeen/EO_PointTest/ListOfBest_Reader.py
@@ -1,8 +1,10 @@
 import numpy as np
 
+filepath = "list_of_bests_PSO_rastrigin.tsv"
+
 # Find minimum run length
 min_len = 0
-with open("list_of_bests_PSO_rastrigin.tsv", "r") as f:
+with open(filepath, "r") as f:
     for i, line in enumerate(f):
         line_array = line.split()
         new = list(map(float, line_array))
@@ -10,3 +12,4 @@ with open("list_of_bests_PSO_rastrigin.tsv", "r") as f:
         if min_len == 0 or line_len < min_len:
             min_len = line_len
 print(min_len)
+
