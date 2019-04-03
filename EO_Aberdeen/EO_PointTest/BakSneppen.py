591f763f6227a562ce2491783e295cf679252adc
Fleford Redoloza <fleford@gmail.com>
2019-04-03 00:36:15 -0600

    started Bak_Sneppen model

--- EO_Aberdeen/EO_PointTest/BakSneppen.py
+++ EO_Aberdeen/EO_PointTest/BakSneppen.py
@@ -12,20 +12,30 @@ print(fitness)
 
 fig, ax = plt.subplots()
 
-ax.set_ylim([0, y_lim])
-bar_plot = plt.bar(x, fitness)
+for run in range(30):
 
-plt.xticks(x)
-plt.xlabel("Creatures")
-plt.ylabel("Fitness of creature")
-bar_plot[np.argmin(fitness)-1].set_color('r')
-bar_plot[np.argmin(fitness)].set_color('r')
-bar_plot[np.argmin(fitness)+1].set_color('r')
+    # plot points
+    ax.clear()
+    bar_plot = plt.bar(x, fitness)
+    plt.xticks(x)
+    ax.set_ylim([0, y_lim])
+    plt.xlabel("Solution Components")
+    plt.ylabel("Fitness of component")
+    plt.title('Iteration = {}, Total fitness = {}'.format(run, np.sum(fitness)))
+    # plt.pause(0.1)
 
-plt.pause(1)
+    # find min index
+    min_index = np.argmin(fitness)
 
+    # color lowest fitness and plot
+    # bar_plot[(min_index - 1) % pop_size].set_color('r')
+    bar_plot[min_index].set_color('r')
+    # bar_plot[(min_index + 1) % pop_size].set_color('r')
 
+    # plot result
+    plt.pause(0.3)
 
-
-ax.clear()
-plt.pause(1)
+    # Adjust fitness
+    # fitness[(min_index - 1) % pop_size] = np.random.randint(low=1, high=y_lim)
+    fitness[min_index] = np.random.randint(low=1, high=y_lim)
+    # fitness[(min_index + 1) % pop_size] = np.random.randint(low=1, high=y_lim)
