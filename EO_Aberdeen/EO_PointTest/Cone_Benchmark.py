ad6aa3f8b6b87161d9d9a52baf779301f2ecbae0
Fleford Redoloza <fleford@gmail.com>
2019-05-23 22:59:13 -0700

    defined the distance function for benchmark

--- EO_Aberdeen/EO_PointTest/Cone_Benchmark.py
+++ EO_Aberdeen/EO_PointTest/Cone_Benchmark.py
@@ -1,24 +1,24 @@
 import numpy as np
 
 
-def sphere_2d(x, y):
+def cone_2d(x, y):
     return x ** 2 + y ** 2
 
 
-def sphere_multi_2d_total(x_array):
+def cone_multi_2d_total(x_array):
     x_array = np.asarray(x_array)
     x_array = x_array.reshape(-1, 2)
 
-    result = sum(sphere_2d(x_array[:, 0], x_array[:, 1]))
+    result = sum(cone_2d(x_array[:, 0], x_array[:, 1]))
 
     return result
 
 
-def sphere_multi_2d_array(x_array):
+def cone_multi_2d_array(x_array):
     x_array = np.asarray(x_array)
     x_array = x_array.reshape(-1, 2)
 
-    result = sphere_2d(x_array[:, 0], x_array[:, 1])
+    result = cone_2d(x_array[:, 0], x_array[:, 1])
     result = result.reshape(-1, 1)
 
     return result
@@ -26,6 +26,6 @@ def sphere_multi_2d_array(x_array):
 
 # Test with one vector
 x1 = [1, 1, 1, 1, 1, 1]
-print(sphere_multi_2d_array(x1))
-print(sphere_multi_2d_total(x1))
+print(cone_multi_2d_array(x1))
+print(cone_multi_2d_total(x1))
 
